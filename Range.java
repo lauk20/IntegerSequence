07bcea307eec720486e7a3bf8e5fd6377bf9e24d
lauk20 <klau20@stuy.edu>
2020-12-04 16:03:52 -0500

    Added reset() method and constructor for Range class. Class cannot be used yet, needs to override more methods found in IntegerSequence interface.

---
+++ Range.java
@@ -0,0 +1,14 @@
+public class Range implements IntegerSequence{
+  private int start, end, current;
+
+  public Range(int start, int end){
+    this.start = start;
+    this.end = end;
+    current = start;
+  }
+
+  public void reset(){
+    current = start;
+  }
+
+}
